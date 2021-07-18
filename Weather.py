c4c1d64c3e4e61049b5f6b2d826cfded7dae29df
Recons7 <whack5065@gmail.com>
2021-07-18 14:51:07 +0530

    First Commit

---
+++ Weather.py
@@ -0,0 +1,42 @@
+import requests
+import time, os, pyfiglet
+
+
+from termcolor import colored
+
+os.system('clear')
+
+print(colored(pyfiglet.figlet_format("Weather"), "green"))
+BOLD = '\033[1m'
+END = '\033[0m'
+
+print(colored("\n\n"+"="*54, "yellow"))
+print(colored(BOLD+'\n\t    WEATHER MAP BY :- SURAJ SHARMA'+END, "blue"))
+print(colored("\n"+"="*54, "yellow"))
+
+def weather():
+	
+	city = input("\n\nEnter your city : ")
+	
+	r = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=ff189edaab5a0d9cf74220087f0b69e9')
+	
+	print("\n")
+	print(colored("\n"+"="*54, "yellow"))
+	print(colored(f"\n\n'{r.json()['weather'][0]['description']}' at {city}", "magenta"))
+	
+	print("\n")
+
+	print(colored(f"\nTemeperature : {int(r.json()['main']['temp']-273.15)}°C\n", "yellow"))
+	
+	print("\n")
+	
+	print(colored(f"Wind speed : {r.json()['wind']['speed']}", "cyan"))
+	
+	print(colored("\n\n"+"="*54, "yellow"))
+	print("\n")
+	
+	
+	#pprint.pprint(r.json())
+	
+	weather()
+weather()
